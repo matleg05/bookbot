12812cf14fd9eee289368167618c922f9da7bcf0
matleg05 <mathieu.leger05@gmail.com>
2024-05-03 14:55:39 +0200

    final functions added to make things work

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
+path = "books/frankenstein.txt"
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in this book")
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-def get_book_text(path):
     with open(path) as f:
-        return f.read()
+        file_contents = f.read()
+        return file_contents
+def count_words():
+    return len(main().split())
+def count_letters():
+    lowercase_letters = main().lower()
+    temp_dic = {}
+    final_list = []
 
-main()
+    for letter in lowercase_letters:
+        if letter.isalpha() == True:
+            if letter in temp_dic:
+                temp_dic[letter] += 1
+            else:
+                temp_dic[letter] = 1
+
+    for i in temp_dic:
+        new_dic = {}
+        new_dic["letter"] = i
+        number = temp_dic[i]
+        new_dic["number"] = number
+        final_list.append(new_dic)
+
+    return final_list
+def report():
+    dic = count_letters()
+    def sort_on(dic):
+        return dic["number"]
+    dic.sort(reverse=True, key=sort_on)
+
+    final_report = f"--- Begin report of {path} --- \n"
+
+    for i in dic:
+        let = i["letter"]
+        num = i["number"]
+        line = f"The {let} character was found {num} times \n"
+        final_report += line
+
+    final_report += "--- End report ---"
+
+    print(final_report)
+
+report()
